d69b107a9d41c5471776cd2b462e1b3189c18d71
samjhooker <samjhooker@gmail.com>
2014-11-20 21:24:51 +1300

    basic containers setup

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -9,15 +9,82 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var numberOfIce:Int = 0
+    var numberOfLemon:Int = 0
+    var numberOfMoney:Int = 100
+    var numberOfCups:Int = 0
+    
+    var titleContainer: UIView!
+    var contentContainer1: UIView!
+    var contentContainer2: UIView!
+    var contentContainer3: UIView!
+    var contentContainer4: UIView!
+    var moneyContainer: UIView!
+    var startContainer: UIView!
+    
+    var kEighth:CGFloat = 1.0/8.0
+    var kHalf:CGFloat = 1.0/2.0
+    var kThird:CGFloat = 1.0/3.0
+    var kSeventh:CGFloat = 1.0/7.0
+    
+    var titleLabel:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.setUpContainerViews()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+    func setUpContainerViews(){
+        
+        self.titleContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x, y: self.view.bounds.origin.y, width: self.view.bounds.width, height: self.view.bounds.height * kEighth))
+        self.titleContainer.backgroundColor = UIColor.yellowColor()
+        self.view.addSubview(self.titleContainer)
+        setUpTitleContainer()
+        
+        self.contentContainer1 = UIView(frame: CGRect(x: self.view.bounds.origin.x + 5, y: self.titleContainer.frame.height + 5, width: self.view.bounds.width * kHalf - 7, height: self.view.bounds.height * 3 * kEighth))
+        self.contentContainer1.backgroundColor = UIColor.lightGrayColor()
+        self.view.addSubview(self.contentContainer1)
+        
+        self.contentContainer2 = UIView(frame: CGRect(x: contentContainer1.frame.width + 10, y: self.titleContainer.frame.height + 5, width: self.view.bounds.width * kHalf - 7, height: self.view.bounds.height * 3 * kEighth))
+        self.contentContainer2.backgroundColor = UIColor.lightGrayColor()
+        self.view.addSubview(self.contentContainer2)
+        
+        self.contentContainer3 = UIView(frame: CGRect(x: self.view.bounds.origin.x + 5, y: self.contentContainer1.frame.height + 10 + titleContainer.frame.height, width: self.view.bounds.width * kHalf - 7, height: self.view.bounds.height * 3 * kEighth))
+        self.contentContainer3.backgroundColor = UIColor.lightGrayColor()
+        self.view.addSubview(self.contentContainer3)
+        
+        self.contentContainer4 = UIView(frame: CGRect(x: contentContainer1.frame.width + 10, y: self.contentContainer1.frame.height + 10 + titleContainer.frame.height, width: self.view.bounds.width * kHalf - 7, height: self.view.bounds.height * 3 * kEighth))
+        self.contentContainer4.backgroundColor = UIColor.lightGrayColor()
+        self.view.addSubview(self.contentContainer4)
+        
+        self.moneyContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x, y: self.contentContainer1.frame.height + 15 + titleContainer.frame.height + contentContainer3.frame.height, width: self.view.bounds.width * kThird, height: self.view.bounds.height * kEighth))
+        self.moneyContainer.backgroundColor = UIColor.darkGrayColor()
+        self.view.addSubview(self.moneyContainer)
+        
+        self.startContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x + moneyContainer.frame.width, y: self.contentContainer1.frame.height + 15 + titleContainer.frame.height + contentContainer3.frame.height, width: self.view.bounds.width * 2 * kThird, height: self.view.bounds.height * kEighth))
+        self.startContainer.backgroundColor = UIColor.yellowColor()
+        self.view.addSubview(self.startContainer)
+    }
+    
+    
+    func setUpTitleContainer(){
+        titleLabel = UILabel()
+        titleLabel.text = "Lemonade Stand"
+        titleLabel.font = UIFont(name: "AmericanTypewriter", size: 32)
+        titleLabel.sizeToFit()
+        titleLabel.center = titleContainer.center
+        titleContainer.addSubview(titleLabel)
     }
 
 
